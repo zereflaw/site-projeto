9361366e6ba54cb60e6d67c1dc5065c9da740d28
Daniel Leal <85421049+zereflaw@users.noreply.github.com>
2021-06-29 12:45:31 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -21,10 +21,103 @@ for (const link of links) {
 const header = document.querySelector('#header')
 const navHeight = header.offsetHeight
 
-window.addEventListener('scroll', function () {
-  if (window.scrollY >= navHeight) {
-    header.classList.add('scroll')
-  } else {
-    header.classList.remove('scroll')
+function changeHeaderWhenScroll() {
+  window.addEventListener('scroll', function () {
+    if (window.scrollY >= navHeight) {
+      header.classList.add('scroll')
+    } else {
+      header.classList.remove('scroll')
+    }
+  })
+}
+
+/* testimonials slider (swiper) */
+const swiper = new Swiper('.swiper-container', {
+  direction: 'horizontal',
+  loop: false,
+
+  pagination: {
+    el: '.swiper-pagination'
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  },
+
+  scrollbar: {
+    el: '.swiper-scrollbar'
+  },
+
+  keyboard: true,
+  mousewheel: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      setWrapperSize: true
+    }
   }
 })
+
+/* scroll reveal */
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `
+#home .text, #home .image,
+#about .image, #about .text,
+#services header, #services .card,
+#testimonials header, #testimonials .testimonials
+#contact .text, #contact links,
+footer .brand, footer .social
+`,
+  { interval: 90 }
+)
+
+/* back to top button */
+
+function backToTop() {
+  const backToTopButton = document.querySelector('.back-to-top')
+
+  if (window.scrollY >= 1250) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
+window.addEventListener('scroll', function () {
+  changeHeaderWhenScroll()
+  backToTop()
+  activeMenuAtCurrentSection()
+})
+
+/* active menu by section */
+const sections = document.querySelectorAll('main section[id]')
+function activeMenuAtCurrentSection() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+  for (const section of sections) {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.add('active')
+    } else {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.remove('active')
+    }
+  }
+}
